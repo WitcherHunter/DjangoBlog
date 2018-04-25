8da01b75be394d6b2d7ac981226f60f43cad0e7b
maodou <gmzhjz@gmail.com>
2018-04-25 14:26:21 +0800

    添加Fabric自动部署脚本

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import env,run
+from fabric.operations import sudo
+
+GIT_REPO = 'https://github.com/WitcherHunter/DjangoBlog.git'
+
+env.user = 'maodou'
+env.password = 'beibeilove'
+
+env.hosts = 'www.yinzimiao.club'
+
+env.port = '22'
+
+def deploy():
+	source_folder = 'home/maodou/sites/www.yinzimiao.club/DjangoBlog'
+
+	run('cd %s && git pull' % source_folder)
+	run("""
+		cd {} &&
+		../Python3/bin/pip install -r requirements.txt &&
+		../Python3/bin/python3 manage.py collectstatic --noinput &&
+		../Python3/bin/python3 manage.py migrate
+		""".format(source_folder))
+	sudo('restart www.yinzimiao.club')
+	sudo('service nginx reload')
